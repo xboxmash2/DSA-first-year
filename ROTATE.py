6d2ba16566f57f96ae797b017e2678bcf9f1d0dc
Dr.Kanika Singh <xboxmash2>
2026-02-13 14:14:10 +0530

    matrix

--- ROTATE.py
+++ ROTATE.py
@@ -1,11 +1,14 @@
 import array
 a=[1,3,5,7,9,11]
 
+for i in a:
+    
+
 k=int(input("enter no."))
 for i in range(k):
     last=a[-1]
     for j in range(len(a)-1,0,-1):
         a[j]=a[j-1]
     a[0]=last
-
+k=k%len(a)
 print(a)
---
+++ matrix.py
@@ -0,0 +1,47 @@
+rows=int(input("enter rows"))
+cols=int(input("enter columns"))
+matrix=[]
+for i in range(rows):
+    arr=[]
+    for j in range(cols):
+        arr.append(int(input("enter value")))
+    matrix.append(arr)
+print(matrix)
+
+
+# for i in matrix:
+#     for j in i:
+#         print(j)
+
+colindex=int(input("enter column index"))
+rowindex=int(input("enter row index"))
+
+print(matrix[rowindex][colindex])
+
+for i in range(len(matrix[0])): #diagonal left to right PRIMARY
+    print(matrix[i][i],end="")
+
+print("\n")
+
+for i in range(len(matrix[0])): #diagonal right to left SECONDARY
+    print(matrix[i][-i-1],end="")
+
+print("\n")
+
+for i in matrix:
+    print(sum(i))
+
+#diagonal SUM PRIMARY
+sum=0
+for i in range(rows):
+    sum+=matrix[i][i]
+print("PRIMARY SUM =",sum)
+
+#diagonal SUM SECONDARY
+sum=0
+for i in range(rows):
+    sum+=matrix[i][cols-1-i]
+print("SECONDARY SUM =",sum)
+
+
+
